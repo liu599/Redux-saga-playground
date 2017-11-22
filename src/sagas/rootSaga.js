04dd19a291d052cd7a67a81288826b304632f90d
Tokei <970228409@qq.com>
2017-11-23 03:06:42 +0800

    update

--- src/sagas/rootSaga.js
+++ src/sagas/rootSaga.js
@@ -10,8 +10,11 @@ export default function* rootSaga() {
   yield [
     fork(loadUser),
     // NOTE: sequenced loading
+    // NOTE: the last two request is only depended on schedule, so this can be done synchronously.
     // takeLatest('LOAD_DASH_BOARD', loadDashBoardSequenced),
     // NOTE: non-blocking (synchronously loading)
+    // NOTE: however, the last two UIs are waiting until the end of the last response
+    // NOTE: what if we need to update the UI as soon as operation is finished?
     takeLatest('LOAD_DASH_BOARD', loadDashBoardNonBlocking),
   ]
 }
